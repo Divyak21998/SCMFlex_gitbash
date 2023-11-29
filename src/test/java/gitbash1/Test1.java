0afb9f483fefbcb99f053920e92d2ef8cc28b449
Divyak21998 <141134125+Divyak21998@users.noreply.github.com>
2023-11-29 09:31:12 +0530

    Update Test1.java

--- src/test/java/gitbash1/Test1.java
+++ src/test/java/gitbash1/Test1.java
@@ -4,6 +4,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("----test1----");
 		System.out.println("---test2----");
+		System.out.println("---test3----");
+		System.out.println("---test4----");
+		
 	}
 
 }
